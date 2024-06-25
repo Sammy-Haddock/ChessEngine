e7fd379b3f7b36b66c8a3c0de877a5d9a15674d0
SammyH2 <91068957+SammyH2@users.noreply.github.com>
2024-06-25 13:02:30 +0100

    Main file

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is a tab:\tand this is after a tab.\n");
+    printf("This is a backslash: \\\n");
+
+    return 0;
+}
